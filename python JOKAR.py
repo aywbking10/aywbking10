73448cbc4bbf5efc7ae9de16bebc868fb7d52287
aywbking10 <116024253+aywbking10@users.noreply.github.com>
2022-10-17 20:27:06 +0300

    python JOKAR.py

--- python JOKAR.py
+++ python JOKAR.py
@@ -7,4 +7,11 @@
 <!---
 aywbking10/aywbking10 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
---->
+---> pkg update && pkg upgrade
+pip install bs4
+pip install requests
+
+pip install mechanize
+git add README.md
+https://github.com/aywbking10/python-JOKAR.py
+cd JOKAR
